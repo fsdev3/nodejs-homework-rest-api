58f9d9f660a70a94f73decef68f002f61a9864c9
Sergiy Nikolaev <122089422+fsdev3@users.noreply.github.com>
2023-10-18 17:18:49 +0200

    fix

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -11,7 +11,7 @@ const { JWT_SECRET } = process.env;
 const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers; // if no token: = "" - postman: "message": "Unauthorized"
   const [bearer, token] = authorization.split(" ");
-  if (bearer !== "Bearer") {
+  if (bearer !== "Bearer" || !token) {
     throw HttpError(401);
   }
 
